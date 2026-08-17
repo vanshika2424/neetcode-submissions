d10e47d36ee2c34120df483df8b15cd58c3fd930
vanshika2424 <165314847+vanshika2424@users.noreply.github.com>
2026-08-17 14:24:41 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(int i,vector<int>& dp){
+        if(i<=1){
+            return 1;
+        }
+
+        if(dp[i]!=-1) return dp[i];
+        return dp[i]=solve(i-1,dp)+solve(i-2,dp); 
+
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        
+        return solve(n,dp);
+        
+    }
+};
